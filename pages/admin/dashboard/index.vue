a23869cffd1e683685e8e0c8769aada81052b63a
deolorensa <lorensadeo10@gmail.com>
2022-06-10 22:04:06 +0700

    first commit admin

---
+++ pages/admin/dashboard/index.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+    <div class="flex flex-row justify-center mx-9 p-5 ">
+        <div class="basis-1/3 text-center w-full mx-4 block m-3 p-6 bg-white  border-4 border-red-500 shadow-xl">
+            <p class="mb-2 text-xl font-bold tracking-tight text-black dark:text-white">Active Membership</p>
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 self-center" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+            </svg>
+            <p class="font-normal text-black dark:text-gray-400">1000</p>
+        </div>
+        <div class="basis-1/3 text-center w-full mx-4 block m-3 p-6  bg-white  border-4 border-red-500 shadow-xl">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-black dark:text-white">Trainers</h5>
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+            </svg>
+            <p class="font-normal text-black dark:text-gray-400">1000</p>
+        </div>
+        <div class="basis-1/3 text-center w-full mx-4 block m-3 p-6  bwhite  border-4 border-red-500 shadow-xl">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-black dark:text-white">Content</h5>
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 item" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+            </svg>
+            <p class="font-normal text-black dark:text-gray-400">1000</p>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
