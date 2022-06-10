7b5f23081c2d0b81e39d753a31a8b8ff965a99f3
deolorensa <lorensadeo10@gmail.com>
2022-06-10 22:12:08 +0700

    fix layout

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  
+<div class="flex">
+  <sidebar-admin/>
+    <div class="w-full">
+    <Nuxt />
+  </div>
+</div>
+</template>
+
+<script>
+import SidebarAdmin from "~/components/admin/SidebarAdmin.vue"
+
+export default {
+  components: { SidebarAdmin },
+  
+}
+</script>
