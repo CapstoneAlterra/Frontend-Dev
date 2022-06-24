d5b24d6b9cf00cbf57cf49780a54d58803440fc2
Rici Januardi <ricijanuardi@gmail.com>
2022-06-24 15:08:11 +0700

    add some update

---
+++ pages/admin/offlineClass/category/_id.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="mx-5 my-10">
+    <!-- button -->
+    <nuxt-link to="../">
+      <button class="border rounded-full">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-10 w-10"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M15 19l-7-7 7-7"
+          />
+        </svg>
+      </button>
+    </nuxt-link>
+    <!-- card -->
+    <div class="flex">
+      <div
+        class="block m-3 p-5 w-55 h-28 border-4 border-red-500 shadow-md bg-red-100"
+      >
+        <h5 class="mb-2 text-xl px-5 text-center">Cardio</h5>
+        <p class="text-2xl text-center font-bold">3</p>
+      </div>
+    </div>
+    <!-- divider -->
+    <div class="flex justify-between">
+      <h1 class="text-5xl font-medium">Classes</h1>
+      <nuxt-link to="/admin/offlineClass/addClass">
+        <button
+          class="inline-flex bg-red-600 text-white rounded-md gap-1 h-14 w-36 items-center px-1"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 4v16m8-8H4"
+            />
+          </svg>
+          <span class="font-size">Add New Class</span>
+        </button>
+      </nuxt-link>
+    </div>
+    <!-- Classes -->
+    <div class="grid my-5 border-1 border border-red-300 grid-cols-4">
+      <div v-for="(category, index) in categories" :key="index" class="">
+        {{ category.name }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      categories: [
+        {
+          name: "Body Combat",
+          classes: [
+            {
+              trainer: "Trainer Iwan",
+              date: "17 June 2022",
+              time: "20-00 - 21.00",
+              peserta: "30",
+            },
+            {
+              trainer: "Trainer Tulus",
+              date: "17 June 2022",
+              time: "21-00 - 22.00",
+              peserta: "30",
+            },
+          ],
+        },
+        {
+          name: "Step Up",
+          classes: [
+            {
+              trainer: "Trainer Adi",
+              date: "17 June 2022",
+              time: "20-00 - 21.00",
+              peserta: "30",
+            },
+            {
+              trainer: "Trainer Wawan",
+              date: "17 June 2022",
+              time: "21-00 - 22.00",
+              peserta: "30",
+            },
+          ],
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
--- pages/admin/offlineClass/index.vue
+++ pages/admin/offlineClass/index.vue
@@ -5,12 +5,12 @@
       <div
         v-for="(category, index) in categories"
         :key="index"
-        class="block m-3 p-5 max-w-sm bg-white border-4 border-red-500 shadow-md hover:bg-red-100"
+        class="block m-3 p-5 w-50 h-30 bg-white border-4 border-red-500 shadow-md hover:bg-red-100"
       >
         <nuxt-link :to="`/admin/offlineClass/category/${index}`">
           <h5 class="mb-2 text-xl px-5 text-center">{{ category.name }}</h5>
-          <p class="text-5xl text-center font-bold">
-            {{ category.class }}
+          <p class="text-2xl text-center font-bold">
+            {{ category.class }} CLASS
           </p>
         </nuxt-link>
       </div>
@@ -29,7 +29,7 @@
       </div>
       <button
         type="submit"
-        class="items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-red-600 rounded hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        class="items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-red-600 rounded hover:bg-red-400"
       >
         Search
       </button>
@@ -37,13 +37,27 @@
     <!-- dropdown -->
     <div class="my-5 mx-3">
       <button
-        id="dropdownDefault"
-        data-dropdown-toggle="dropdown"
-        class="flex text-black w-60 bg-white border border-red-300 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center space-x-40"
+        @click="dropcategory = !dropcategory"
+        class="flex text-black w-60 bg-white border border-red-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center space-x-40"
         type="button"
       >
         <p>ALL</p>
         <svg
+          v-if="dropcategory"
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          stroke-width="2"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+            clip-rule="evenodd"
+          />
+        </svg>
+        <svg
+          v-else
           xmlns="http://www.w3.org/2000/svg"
           class="h-6 w-6"
           fill="none"
@@ -60,47 +74,66 @@
       </button>
 
       <div
+        v-show="dropcategory"
         id="dropdown"
-        class="z-10 hidden bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700"
-        data-popper-placement="top"
-        style="
-          position: absolute;
-          inset: auto auto 0px 0px;
-          margin: 0px;
-          transform: translate(352px, -12px);
-        "
+        class="z-10 absolute bg-white divide-y divide-gray-100 rounded shadow w-60"
       >
-        <ul
-          class="py-1 text-sm text-gray-700 dark:text-gray-200"
-          aria-labelledby="dropdownDefault"
+        <ul class="py-1 text-sm text-gray-700 dark:text-gray-200">
+          <li v-for="(category, index) in categories" :key="index">
+            <a href="#" class="block px-4 py-2 hover:bg-red-200">{{
+              category.name
+            }}</a>
+          </li>
+        </ul>
+      </div>
+
+      <button
+        @click="dropclass = !dropclass"
+        class="flex text-black w-60 bg-white border border-red-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center space-x-40"
+        type="button"
+      >
+        <p>ALL</p>
+        <svg
+          v-if="dropclass"
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          stroke-width="2"
         >
-          <li>
-            <a
-              href="#"
-              class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-              >Dashboard</a
-            >
-          </li>
-          <li>
-            <a
-              href="#"
-              class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-              >Settings</a
-            >
-          </li>
-          <li>
-            <a
-              href="#"
-              class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-              >Earnings</a
-            >
-          </li>
-          <li>
-            <a
-              href="#"
-              class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-              >Sign out</a
-            >
+          <path
+            fill-rule="evenodd"
+            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+            clip-rule="evenodd"
+          />
+        </svg>
+        <svg
+          v-else
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M9 5l7 7-7 7"
+          />
+        </svg>
+      </button>
+
+      <div
+        v-show="dropclass"
+        id="dropclass"
+        class="m-x-10 z-10 absolute bg-white divide-y divide-gray-100 rounded shadow w-60"
+      >
+        <ul class="py-1 text-sm text-gray-700">
+          <li v-for="(kelas, index) in classes" :key="index">
+            <a href="#" class="block px-4 py-2 hover:bg-red-200">{{
+              kelas.name
+            }}</a>
           </li>
         </ul>
       </div>
@@ -132,35 +165,39 @@
             <td>{{ list.class }}</td>
             <td>{{ list.date }}</td>
             <td>
-              <div class="block">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  stroke-width="2"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
-                  />
-                </svg>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  stroke-width="2"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                  />
-                </svg>
+              <div class="flex justify-around">
+                <button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                    />
+                  </svg>
+                </button>
+                <button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                    />
+                  </svg>
+                </button>
               </div>
             </td>
           </tr>
@@ -173,11 +210,14 @@
 export default {
   data() {
     return {
+      dropclass: false,
+      dropcategory: false,
       categories: [
         { name: "Cardio", class: "3" },
         { name: "Strength", class: "3" },
         { name: "Dance", class: "3" },
       ],
+      classes: [{ name: "Bodycombat" }, { name: "Step up" }],
       lists: [
         {
           id: "0001",
