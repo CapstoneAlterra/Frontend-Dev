53e59affdf03cb35dde6d1d0963516d488cdd55e
Rici Januardi <ricijanuardi@gmail.com>
2022-07-04 12:26:22 +0700

    first commit

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,63 +1,104 @@
 <template>
-<div class="container m-auto mt-10">
-  <div class="p-4 mx-auto max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
+  <div class="container m-auto mt-10">
+    <div
+      class="p-4 mx-auto max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700"
+    >
       <div class="flex justify-center my-7">
-        <img src="@/static/Logo.png" alt="">
+        <img src="@/static/Logo.png" alt="" />
       </div>
-        <div class="my-7">
-            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-            <input v-model="email" type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required>
-        </div>
-        <div class="my-8">
-            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your password</label>
-            <input v-model="password" type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
-        </div>
-        <button @click="login()" type="button" class="w-full text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-lg py-2 text-center">Login</button>
+      <div class="my-7">
+        <label
+          for="email"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >Your email</label
+        >
+        <input
+          v-model="login.email"
+          type="email"
+          name="email"
+          id="email"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+          placeholder="name@company.com"
+          required
+        />
+      </div>
+      <div class="my-8">
+        <label
+          for="password"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >Your password</label
+        >
+        <input
+          v-model="login.password"
+          type="password"
+          name="password"
+          id="password"
+          placeholder="••••••••"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+          required
+        />
+      </div>
+      <button
+        @click="userLogin()"
+        type="button"
+        class="w-full text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-lg py-2 text-center"
+      >
+        Login
+      </button>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
 export default {
-    middleware: 'notAuthenticated',
-    data() {
-      return {
-          email: '',
-          password: ''
+  middleware: "notAuthenticated",
+  data() {
+    return {
+      login: {
+        email: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    async userLogin() {
+      try {
+        let response = await this.$auth
+          .loginWith("local", { data: this.login })
+          .then((result) => {
+            this.alertLoginSucces();
+            this.$auth.setUserToken(
+              result.data.result.token,
+              result.data.result.refresh_token
+            );
+            console.log(response);
+            console.log(result);
+            this.$router.replace({ name: "admin-dashboard" });
+          })
+          .catch((error) => {
+            this.alertLoginError(error);
+            console.log(err);
+          });
+      } catch (error) {
+        this.alertLoginError(error);
       }
     },
-    methods: {
-      async login () {
-        const response = await this.$axios.$post('https://gym.stack.co.id/member/auth/login', 
-        {email: this.email, password: this.password
-        })
-        .then((response) => {
-          this.alertLoginSucces()
-          this.$store.commit("loginToken", response.result.token)
-          this.$router.replace({name:'admin-dashboard'})
-        })
-        .catch((error) => {
-          this.alertLoginError(error)
-        })
-      },
-        alertLoginSucces(){
-          this.$swal({
-            position: 'center',
-            title: 'Log In Succes',
-            timer: 1500
-            });
-          },
-        alertLoginError(){
-          this.$swal({
-            position: 'center',
-            title: 'Login Failed, Username or Password Wrong !!!',
-            timer: 1500
-            });
-          },
+    alertLoginSucces() {
+      this.$swal({
+        position: "center",
+        title: "Log In Succes",
+        timer: 1500,
+      });
+    },
+    alertLoginError() {
+      this.$swal({
+        position: "center",
+        title: "Login Failed, Username or Password Wrong !!!",
+        timer: 1500,
+      });
+    },
   },
-}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
